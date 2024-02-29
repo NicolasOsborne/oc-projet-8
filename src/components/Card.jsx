7dcdfa098fb67007f8d3563252ec4b602c98dedd
Nicolas Osborne <nho.osborne@gmail.com>
2024-02-29 11:43:38 +0100

    Récupération des données du JSON pour générer les Cards de la page d'accueil (cover, title)

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,13 @@
-const Card = () => {
+const Card = ({ cover, title }) => {
   return (
     <div className='rental-card'>
-      <h3 className='rental-name'>Titre de la location</h3>
+      <img
+        className='rental-cover'
+        src={cover}
+        alt="Photo d'illustration de la location"
+      ></img>
+      <div className='rental-mask'></div>
+      <h3 className='rental-title'>{title}</h3>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,15 @@
 import Banner from '../components/Banner'
 import Card from '../components/Card'
+import rentalsList from '../data/rentals.json'
 
 function Home() {
   return (
     <>
       <Banner />
       <div className='rental-card-container'>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {rentalsList.map((rental) => (
+          <Card key={rental.id} cover={rental.cover} title={rental.title} />
+        ))}
       </div>
     </>
   )
