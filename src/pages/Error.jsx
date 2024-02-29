f9ad60083cb362bb99a0b36f648724fe4aa6da0e
Nicolas Osborne <nho.osborne@gmail.com>
2024-02-29 16:36:36 +0100

    Ajout du responsive Mobile & Tablet sur la page Error

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -4,7 +4,10 @@ function Error() {
   return (
     <div className='error-container'>
       <h1>404</h1>
-      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <p>
+        Oups! La page que <br className='error-break' />
+        vous demandez n'existe pas.
+      </p>
       <Link to='/'>Retourner sur la page d'accueil</Link>
     </div>
   )
