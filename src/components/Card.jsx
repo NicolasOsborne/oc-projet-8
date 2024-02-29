61b45bd0b2b101b26085289288f301854324db56
Nicolas Osborne <nho.osborne@gmail.com>
2024-02-29 10:52:06 +0100

    Création du composant Card - Ajout temporaire de 6 Cards manuellement pour la mise en page du container de la page Home

---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+const Card = () => {
+  return (
+    <div className='rental-card'>
+      <h3 className='rental-name'>Titre de la location</h3>
+    </div>
+  )
+}
+
+export default Card
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,20 @@
 import Banner from '../components/Banner'
+import Card from '../components/Card'
 
 function Home() {
-  return <Banner />
+  return (
+    <>
+      <Banner />
+      <div className='rental-card-container'>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </>
+  )
 }
 
 export default Home
